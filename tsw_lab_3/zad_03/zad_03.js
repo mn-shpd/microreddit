cd7d03ef8dc49f3e7283e48765ea3a6ddb831b75
mn.shpd <thewojtekr@gmail.com>
2021-03-16 20:44:37 +0100

    zad03

--- tsw_lab_3/zad_03/zad_03.js
+++ tsw_lab_3/zad_03/zad_03.js
@@ -1,17 +1,15 @@
 const groupBy = (tab, key) => {
-    let trueTab = [];
-    let falseTab = [];
-    tab.forEach(el => {
-        if(key(el)) {
-            trueTab.push(el);
-        } else {
-            falseTab.push(el);
-        }
-    });
 
-    let mapa = new Map();
-    mapa.set(true, trueTab);
-    mapa.set(false, falseTab);
+    let mapa = tab.reduce((prev, curr) => {
+        if(prev.get(key(curr))) {
+            let tempTab = prev.get(key(curr));
+            tempTab.push(curr);
+            prev.set(key(curr), tempTab);
+        } else {
+            prev.set(key(curr), [curr]);
+        }
+        return prev;
+    }, new Map());
 
     return mapa;
 };
