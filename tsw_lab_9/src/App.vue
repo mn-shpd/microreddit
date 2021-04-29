eb047c83a29cd6eb75da72cef86a8219266e5239
mn.shpd <thewojtekr@gmail.com>
2021-04-29 09:43:45 +0200

    zad01

---
+++ tsw_lab_9/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <post v-for="(post, index) in posts" :key="post" :id="index" :content="post" @deleteEvent="deletePost"></post>
+  </ul>
+</template>
+
+<script>
+import Post from './components/Post.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Post
+  },
+  data () {
+    return {
+      posts: ["Pierwszy post", "Drugi post", "Trzeci post"]
+    }
+  },
+  methods: {
+    deletePost(id) {
+      this.posts.splice(id, 1);
+    }
+  }
+}
+</script>
+
+<style>
+  ul {
+    padding: 0;
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
---
+++ tsw_lab_9/src/components/Post.vue
@@ -0,0 +1,39 @@
+<template>
+<li>{{ content }} <button @click="deletePost">Usuń</button></li>
+
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  props: {
+    id: Number,
+    content: String
+  },
+  emits: {
+    deleteEvent: () => {
+        console.log("Weryfikacja eventu pomyślna!");
+        return true;
+    }
+  },
+  methods: {
+      deletePost() {
+          this.$emit('deleteEvent', this.id);
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+li {
+    background-color: bisque;
+    border: 2px solid black;
+    margin: 5px;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    width: 50%;
+}
+
+</style>
