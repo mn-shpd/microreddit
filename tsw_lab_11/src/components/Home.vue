c3fd373bb3b780cc29b38d05eee17d250baf2c78
mn.shpd <thewojtekr@gmail.com>
2021-05-15 12:57:09 +0200

    lab_11 zad_01 finished

--- tsw_lab_11/src/components/Home.vue
+++ tsw_lab_11/src/components/Home.vue
@@ -2,7 +2,7 @@
   <div id="content">
     <h1>THREE RANDOM TASKS</h1>
     <ul>
-      <task v-for="task in randomTasks" :key="task" :id="task.id" :content="task.task" :done="task.done" @reloadEvent="loadTasks"></task>
+      <task v-for="task in randomUndoneTasks" :key="task" :id="task.id" :content="task.task" :done="task.done" @reloadEvent="loadTasks"></task>
     </ul>
     <router-link to="/list">All tasks</router-link>
     <router-link to="/new">Add a new task</router-link>
@@ -19,13 +19,11 @@ export default {
   },
   data () {
     return {
-      tasks: [],
-      randomTasks: []
+      tasks: []
     }
   },
   created: function () {
     this.loadTasks();
-    this.getRandomTasks();
   },
   methods: {
     async loadTasks() {
@@ -33,20 +31,19 @@ export default {
       let temp_tasks = await axios.get("http://localhost:3000/tasks/");
       this.tasks = temp_tasks.data;
       this.tasks.sort((a, b) => (a.id > b.id) ? 1 : -1);
-    },
-    getRandomTasks() {
+    }
+  },
+  computed: {
+    randomUndoneTasks: function() {
       let undonetasks = this.tasks.filter(el => el.done === false);
-      console.log(undonetasks);
-      let result = [];
 
       if(undonetasks.length > 3) {
-        this.randomTasks = undonetasks;
-      } else {
-        while(result.length !== 3) {
-          result.push(undonetasks.splice(Math.floor(Math.random() * undonetasks.length), 1));
+        while(undonetasks.length !== 3) {
+          undonetasks.splice(Math.floor(Math.random() * undonetasks.length), 1);
         }
       }
-      this.randomTasks = result;
+
+      return undonetasks;
     }
   }
 }
